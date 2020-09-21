1bc2e4b20aebc59114fbd6629e652af710b0a698
Tom Timmerman <info@tomtimmerman.nl>
2020-09-21 19:11:56 +0200

    added validator

--- src/OrderValidator.ts
+++ src/OrderValidator.ts
@@ -75,8 +75,8 @@ export class IsInFinalState implements OrderValidator {
 
 export class IsProcessingPayment implements OrderValidator {
     validate(order: Order): Boolean {
-        // const hasPayments = order.payments ? order.payments.length > 0 : false;
-        return order && order.status === OrderStatus.checkOut && order.paymentStatus !== PaymentStatus.paid;
+        const hasPayments = order && order.payments ? order.payments.length > 0 : false;
+        return order && order.status === OrderStatus.checkOut && order.paymentStatus !== PaymentStatus.paid && hasPayments;
     }
 }
 
